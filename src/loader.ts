4ab95cda4ac8f4aa9a1b9e20070c7327b30c42af
timoteostifft <timoteostifft@gmail.com>
2023-08-17 16:02:54 -0300

    feat: add documments chunk splitter

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,8 @@
 import path from 'node:path'
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
 import { JSONLoader } from 'langchain/document_loaders/fs/json'
+import { TokenTextSplitter } from 'langchain/text_splitter'
+import { createClient } from 'redis'
 
 const loader = new DirectoryLoader(
   path.resolve(__dirname, '../tmp'),
@@ -12,7 +14,15 @@ const loader = new DirectoryLoader(
 async function load() {
   const docs = await loader.load()
 
-  console.log(docs)
+  const splitter = new TokenTextSplitter({
+    encodingName: 'cl100k_base',
+    chunkSize: 1,
+    chunkOverlap: 0
+  })
+
+  const splittedDocumments = await splitter.splitDocuments(docs)
+
+  console.log(splittedDocumments)
 }
 
 load();
